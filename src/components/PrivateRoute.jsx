95d36c005e73d4c3be59d3d07bcab5fb632d3834
RafidMuhymin <rafidmuhymin@gmail.com>
2022-01-05 22:06:32 +0600

    removed PrivateRoute.jsx

--- src/components/PrivateRoute.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Navigate } from "react-router-dom";
-
-export default function PrivateRoute({ user, element, ...props }) {
-  if (user.uid) {
-    return element;
-  }
-  return <Navigate to="/login" />;
-}
